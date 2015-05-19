88ce577bde5dcdcad2e86a17b023627349ab934e
cevdetdal <dal.cevdet@gmail.com>
2015-05-19 12:17:54 +0300

    Merge branch 'master' of https://github.com/LeanAcademy/terp

---
+++ terp/src/terp/data/model/EmployeeGroup.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright (C) 2014 ilknur
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package terp.data.model;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+/**
+ * employee group entity
+ * @author ilknur
+ */
+@Entity
+@Table(name="grup",catalog="terp", uniqueConstraints = {
+        @UniqueConstraint(name="pk_grup", columnNames="ref_num"),
+        @UniqueConstraint(name="ix_grup", columnNames="grup_adi")
+    })
+@NamedQueries ({
+    @NamedQuery(name="EMPLOYEEGROUP_FIND_ALL", 
+            query="from EmployeeGroup eg"),
+    @NamedQuery(name="EMPLOYEEGROUP_FIND_BY_PK", 
+            query="from EmployeeGroup eg "
+            + "where eg.rowid = :id")
+})
+public  class EmployeeGroup implements Serializable {
+
+    private Long rowid;
+    private String groupName;
+    private int status;
+    private Set<Employee> employee = new HashSet<>();
+    
+    public EmployeeGroup(){
+    }
+    
+    
+    @Id
+    @Column(name="ref_num", nullable=false)
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    public Long getRowid() {
+        return this.rowid;
+    }
+
+    public void setRowid (Long rowid) {
+        this.rowid = rowid;
+    }
+    
+    @Column(name="grup_adi",nullable=false,length=50)
+    public String getGroupName() {
+        return this.groupName;
+    }
+
+    public void setGroupName (String groupName) {
+        this.groupName = groupName;
+    }
+
+    @Column(name="durum")
+    public int getStatus(){
+        return this.status;
+    }
+    
+    public void setStatus(int status){
+        this.status = status;
+    }
+    
+    @OneToMany(fetch=FetchType.LAZY, mappedBy="group")
+    public Set<Employee> getEmployee(){
+        return this.employee;
+    }
+    
+    public void setEmployee(Set<Employee> employee){
+        this.employee = employee;
+    }
+
+}
+
---
+++ terp/src/terp/gui/SplashScreen.java
@@ -0,0 +1,135 @@
+/*
+ * Copyright (C) 2014 ilknur
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package terp.gui;
+
+import java.awt.*;
+import javax.swing.*;
+
+/**
+ *
+ * @author ilknur
+ */
+
+public class SplashScreen extends JWindow {
+    
+    //local variables
+    BorderLayout borderLayout1 = new BorderLayout();
+    JLabel imageLabel = new JLabel();
+    JPanel southPanel = new JPanel();
+    FlowLayout southPanelFlowLayout = new FlowLayout();
+    JProgressBar progressBar = new JProgressBar();
+    ImageIcon imageIcon;
+    
+    /**
+     * 
+     * @param imageIcon 
+     */
+    public SplashScreen() {
+      imageIcon = new ImageIcon(getClass().
+              getResource("/terp/resources/images/splashscreen.png"));
+      try {
+        Initialize();
+      }
+      catch(Exception ex) {
+        ex.printStackTrace();
+      }
+    }
+
+    /**
+     * 
+     * @throws Exception 
+     */
+    void Initialize() throws Exception {
+      imageLabel.setIcon(imageIcon);
+      this.getContentPane().setLayout(borderLayout1);
+      southPanel.setLayout(southPanelFlowLayout);
+      southPanel.setBackground(Color.BLACK);
+      this.getContentPane().add(imageLabel, BorderLayout.CENTER);
+      this.getContentPane().add(southPanel, BorderLayout.SOUTH);
+      southPanel.add(progressBar, null);
+      this.pack();
+    }  
+    
+    /**
+     * 
+     * @param maxProgress 
+     */
+    public void setProgressMax(int maxProgress){
+      progressBar.setMaximum(maxProgress);
+    }
+
+    /**
+     * 
+     * @param progress 
+     */
+    public void setProgress(int progress){
+      final int theProgress = progress;
+      SwingUtilities.invokeLater(new Runnable() {
+        public void run() {
+          progressBar.setValue(theProgress);
+        }
+      });
+    }
+
+    /**
+     * 
+     * @param message
+     * @param progress 
+     */
+    public void setProgress(String message, int progress){
+      final int theProgress = progress;
+      final String theMessage = message;
+      setProgress(progress);
+      SwingUtilities.invokeLater(new Runnable() {
+        public void run() {
+          progressBar.setValue(theProgress);
+          setMessage(theMessage);
+        }
+      });
+    }
+
+    /**
+     * 
+     * @param b 
+     */
+    public void setScreenVisible(boolean b){
+      final boolean boo = b;
+      SwingUtilities.invokeLater(new Runnable() {
+        public void run() {
+          setVisible(boo);
+        }
+      });
+    }
+
+    /**
+     * 
+     * @param message 
+     */
+    private void setMessage(String message){
+      if (message==null)
+      {
+        message = "";
+        progressBar.setStringPainted(false);
+      }
+      else
+      {
+        progressBar.setStringPainted(true);
+      }
+      progressBar.setString(message);
+    }    
+}
